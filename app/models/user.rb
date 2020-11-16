65b1b5fae9d19a967e4c93a2e10003e7afa164d6
siga-siga <si5415@outlook.jp>
2020-11-16 19:35:12 +0900

    Revert "バリデーションの設定"
    This reverts commit 73efd1d7b32f9483b092076c3af7fee16e67c375.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true, length: { maximum: 40 }
-  validates :birthday, presence: true
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
-  validates :last_name
-  validates :first_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-  validates :last_name_kana
-  validates :first_name_kana
-  end
-
-  VALID_PASSWORD_REGEX =  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
-
 end
