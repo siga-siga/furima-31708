f1bc43920cc911c6fbdb99cf4961c62b0949873b
siga-siga <si5415@outlook.jp>
2020-11-21 22:24:46 +0900

    単体テストコード

---
+++ app/models/purchase_form.rb
@@ -0,0 +1,21 @@
+class PurchaseForm < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :phone, :item_id, :user_id 
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 1}
+    validates :city
+    validates :block
+    validates :phone, format: {with: /\A\d{10,11}\z/ }
+    validates :item_id
+    validates :user_id
+  end
+
+  def save
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone: phone)
+  end
+end
+
+
